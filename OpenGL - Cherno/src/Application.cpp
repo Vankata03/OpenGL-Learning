35e2d5bcd7e6d39f11dc6086fbd9a8dc63008956
Vankata <ivanterziev38@gmail.com>
2025-12-27 22:19:55 +0200

    Draw 2 rectangles now

--- OpenGL - Cherno/src/Application.cpp
+++ OpenGL - Cherno/src/Application.cpp
@@ -50,14 +50,24 @@ int main(void)
     // OpenGL version print
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// Setup ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui_ImplOpenGL3_Init("#version 330");
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui::StyleColorsDark();
+
 	// We scope so that destructor are called before glfwTerminate and we don't get the "invalid context" errors
     {
-        // Define positions for a triangle (vertex positions)
         float positions[] = {
-              100.0f, 100.0f, 0.0f, 0.0f,
-              200.0f, 100.0f, 1.0f, 0.0f,
-			  200.0f, 200.0f, 1.0f, 1.0f,
-			  100.0f, 200.0f, 0.0f, 1.0f
+              -50.0f, -50.0f, 0.0f, 0.0f,
+               50.0f, -50.0f, 1.0f, 0.0f,
+			   50.0f,  50.0f, 1.0f, 1.0f,
+			  -50.0f,  50.0f, 0.0f, 1.0f
         };
 
         // Define an index array
@@ -84,12 +94,16 @@ int main(void)
 
 		// TODO: Move to perspective - Create a projection matrix
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
 		// Create and bind a shader
         Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/wood.jpg");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		// TODO: Remove - Unbind everything
         va.Unbind();
@@ -99,20 +113,10 @@ int main(void)
 
 		Renderer renderer;
 
-		// Setup ImGui context
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-
-		ImGui_ImplOpenGL3_Init("#version 330");
-		ImGui_ImplGlfw_InitForOpenGL(window, true);
-        ImGui::StyleColorsDark();
-
         float r = 0.0f;
         float increment = 0.05f;
-        glm::vec3 translation = glm::vec3(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -122,18 +126,22 @@ int main(void)
 
 			ImGui_ImplGlfw_NewFrame();
 
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-			glm::mat4 mvp = proj * view * model;
-
             shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
 
-			Texture texture("res/textures/wood.jpg");
-            texture.Bind();
-            shader.SetUniform1i("u_Texture", 0);
+            { 
+			    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+			    glm::mat4 mvp = proj * view * model;
+			    shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            renderer.Draw(va, ib, shader);
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
             if (r > 1.0f)
                 increment = -0.05f;
@@ -148,7 +156,8 @@ int main(void)
 			ImGui::NewFrame();
 
 			// ImGui UI code
-			ImGui::SliderFloat3("float", &translation.x, 0.0f, 500.0f);
+			ImGui::SliderFloat3("float", &translationA.x, 0.0f, 500.0f);
+            ImGui::SliderFloat3("float2", &translationB.x, 0.0f, 500.0f);
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             
 			// Render the ImGui frame
